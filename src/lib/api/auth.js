20f5a8a4d1b73e39f8b3429889576b243a0f107b
ShinChanU <tlscksdn963@naver.com>
2022-01-19 14:22:32 +0900

    rest api update

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -6,7 +6,11 @@ export const login = ({ userId, password }) =>
 
 // 회원가입
 export const signup = ({ userId, password, passwordCheck, username, nickname, birthday, phoneNum, gender, email }) =>
-  client.post('/api/signup', { userId, password, passwordCheck, username, nickname, birthday, phoneNum, gender, email });
+  client.post('/api/signup', {
+    userId, password, passwordCheck, username, nickname, birthday, phoneNum, gender, email
+  })
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
 
 // // 로그인 상태 확인
 export const check = () => client.get('/api/auth/check');
